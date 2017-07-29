8ff47061cdd016ee7aa83c0f5d23e7b983fe54e4
pham.biencuong <lentt1991@gmail.com>
2017-07-29 15:40:17 +0800

    Base Router

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+export const routes: Routes = [
+    //Login
+    { path: '',component:LoginComponent },
+
+]
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LoginComponent]
+})
+export class LoginModule { }
---
+++ src/app/main/main.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component';
+import { RoleComponent } from './role/role.component';
+import {mainRoutes} from './main.routes';
+import {RouterModule, Routes} from '@angular/router';
+import {UserModule} from './user/user.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    UserModule,
+    RouterModule.forChild(mainRoutes)
+  ],
+  declarations: [MainComponent, RoleComponent]
+})
+export class MainModule { }
